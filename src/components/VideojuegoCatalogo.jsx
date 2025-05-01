cdb4f581d61133d885623d221b901a981db05d66
De La Fuente Gonzalo <wwwgonzalito@hotmail.com>
2025-05-01 11:19:34 -0300

    Se agrego la busqueda por filtro en video juego catalogo y la paginacion para navegar y visualizar mejor los video juegos en el caso de mucha cantidad

--- src/components/VideojuegoCatalogo.jsx
+++ src/components/VideojuegoCatalogo.jsx
@@ -1,131 +1,246 @@
-import React, { useEffect, useState } from 'react';
-import { useAuth } from '../context/AuthContext';
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
 import { useVideojuego } from '../context/VideojuegoContext';
 import { Link, useNavigate } from 'react-router-dom';
 import { Gamepad2, Pencil, Trash2, Heart } from 'lucide-react';
-import api from '../services/api';
 import Swal from 'sweetalert2';
 import { toast } from 'react-toastify';
-import { useFavorites } from '../context/FavoritesContext'; // Importa el contexto de favoritos
+import { useFavorites } from '../context/FavoritesContext';
 
 const VideojuegoCatalogo = ({ perfil }) => {
-    const { token } = useAuth(); // Obtiene el token del contexto de autenticación
-    const {  eliminarVideojuego } = useVideojuego();// Obtiene la función para eliminar videojuegos del contexto
-    const [localVideojuegos, setLocalVideojuegos] = useState([]); // Estado local para almacenar los videojuegos
-    const navigate = useNavigate();
+  const { videojuegos, paginaActual, totalPaginas, aplicarFiltros, cambiarPagina, eliminarVideojuego } = useVideojuego();
+  // console.log('totalPaginas', totalPaginas);
+  const [filtrosLocales, setFiltrosLocales] = useState({
+    genero: '',
+    plataforma: '',   
+    edadMinima: '',
+  });// Estado local para almacenar los filtros aplicados
+  const navigate = useNavigate();// Navegador para redirigir después de eliminar un videojuego
+  const { addToFavorites } = useFavorites();
 
-    const { addToFavorites } = useFavorites();
+  useEffect(() => {// Efecto para aplicar los filtros locales al cargar el componente
+    if (perfil) {
+      aplicarFiltros(filtrosLocales); // Aplica los filtros locales al cargar el componente
+    }
+  }, [perfil]);
 
-    useEffect(() => {
-        const fetchVideojuegos = async () => {// Función para obtener los videojuegos
-            try {
-                let filtroEdad = '';
-                if (perfil.tipo === 'adolescente') filtroEdad = '13';// Filtra por edad mínima 13
-                else if (perfil.tipo === 'infantil') filtroEdad = '7';// Filtra por edad mínima 7
+  const handleFiltroChange = (e) => {// Manejo de cambios en los filtros
+    const { name, value } = e.target;// Obtiene el nombre y valor del filtro
+    setFiltrosLocales((prev) => ({// Obtiene el estado anterior y actualiza el filtro correspondiente
+      ...prev,// Actualiza el estado de los filtros locales
+      [name]: value,// Actualiza el filtro correspondiente
+    }));
+  };
 
-                const response = await api.get('/videojuegos', {
-                    headers: { 'Authorization': `Bearer ${token}` },
-                    params: { edadMinima: filtroEdad },// Envía el filtro de edad como parámetro
-                });
-                setLocalVideojuegos(response.data);
-            } catch (error) {
-                console.error('Error al obtener los videojuegos:', error);
-                toast.error('Error al cargar los videojuegos');
-            }
-        };
+  const handleAplicarFiltros = () => {
+    aplicarFiltros(filtrosLocales);
+  };
 
-        fetchVideojuegos();
-    }, [perfil, token]);
+  const handleLimpiarFiltros = () => {
+    setFiltrosLocales({// Reinicia los filtros locales a su estado inicial
+      genero: '',
+      plataforma: '',
+      edadMinima: '',
+    });
+    aplicarFiltros({// Aplica los filtros vacíos para mostrar todos los videojuegos
+      genero: '',
+      plataforma: '',
+      edadMinima: '',
+    });
+  };
 
-    const handleEliminar = async (id) => {
-        const result = await Swal.fire({
-            title: '¿Eliminar videojuego?',
-            text: 'Esta acción no se puede deshacer.',
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#e3342f',
-            cancelButtonColor: '#6c757d',
-            confirmButtonText: 'Sí, eliminar',
-            cancelButtonText: 'Cancelar',
-        });
+  const handleEliminar = async (id) => {
+    const result = await Swal.fire({
+      title: '¿Eliminar videojuego?',
+      text: 'Esta acción no se puede deshacer.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#e3342f',
+      cancelButtonColor: '#6c757d',
+      confirmButtonText: 'Sí, eliminar',
+      cancelButtonText: 'Cancelar',
+    });
 
-        if (result.isConfirmed) {
-            try {
-                await eliminarVideojuego(id);
-                toast.success('Videojuego eliminado exitosamente');
-                navigate(`/catalogo/${perfil._id}`, { state: { profile: perfil } });
-            } catch (error) {
-                console.error('Error al eliminar:', error);
-                toast.error('Error al eliminar el videojuego');
-            }
-        }
-    };
+    if (result.isConfirmed) {
+      try {
+        await eliminarVideojuego(id);
+        toast.success('Videojuego eliminado exitosamente');
+        navigate(`/catalogo/${perfil._id}`, { state: { profile: perfil } });// Redirige al catálogo después de eliminar
+      } catch (error) {
+        // console.error('Error al eliminar:', error);
+        toast.error('Error al eliminar el videojuego');
+      }
+    }
+  };
 
-    const handleAddToFavorites = (videojuego) => {
-        addToFavorites(perfil._id, videojuego);
-    };
-    
-    // const favorites = getFavorites(perfil._id);
+  const handleAddToFavorites = (videojuego) => {
+    addToFavorites(perfil._id, videojuego);
+  };
 
-    return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            {localVideojuegos.map((videojuego) => (// Mapea los videojuegos
-                <div
-                    key={videojuego._id}
-                    className="bg-white rounded-2xl shadow-md hover:shadow-xl overflow-hidden transform hover:scale-105 transition duration-300 relative"
-                >
-                    <img
-                        src={videojuego.imgUrl}
-                        alt={videojuego.titulo}
-                        className="h-48 w-full object-cover"
-                    />
-                    <div className="p-4">
-                        <div className="flex items-center justify-between">
-                            {/* titulo */}
-                            <h2 className="text-lg font-bold truncate">{videojuego.titulo}</h2>
-                            <Gamepad2 className="text-blue-500" size={24} />
-                        </div>
-                        <p className="text-sm text-gray-600 mt-1">Género: {videojuego.genero.join(', ')}</p>
-                        <p className="text-sm text-gray-600">Plataforma: {videojuego.plataforma.join(', ')}</p>
-                        <div className="mt-2">
-                            <span className="inline-block bg-blue-100 text-blue-600 text-xs px-2 py-1 rounded-full">
-                                {videojuego.edadMinima}+ años
-                            </span>
-                        </div>
-                        {/* botones */}
-                        {(perfil.tipo === 'adulto' || perfil.tipo === 'adolescente') && (
-                            <div className="mt-4 flex justify-end space-x-2">
-                                <Link
-                                    to={`/videojuegos/editar/${videojuego._id}`}// Enlace para editar videojuego
-                                    state={{ profile: perfil }}// Pasa el perfil al estado
-                                    className="bg-yellow-400 hover:bg-yellow-500 text-white px-3 py-1 rounded-md flex items-center gap-1 text-sm shadow"
-                                >
-                                    <Pencil size={16} />
-                                    Editar
-                                </Link>
-                                <button
-                                    onClick={() => handleEliminar(videojuego._id)}//uso la funcion handleEliminar para eliminar el videojuego
-                                    className="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded-md flex items-center gap-1 text-sm shadow hover:cursor-pointer"
-                                >
-                                    <Trash2 size={16} />
-                                    Eliminar
-                                </button>
-                            </div>
-                        )}
+  if (!videojuegos) {// Verifica si la lista de videojuegos está vacía o no se ha cargado
+    return <div>Cargando...</div>; // Manejo de estado de carga
+  }
 
-                        <button
-                        onClick={() => handleAddToFavorites(videojuego)}
-                        className="text-red-500 mt-2 flex items-center hover:cursor-pointer hover:text-red-800"
-                        >
-                            <Heart className="mr-1" size={20} /> Agregar a Favoritos
-                        </button>
 
-                    </div>
-                </div>
-            ))}
+
+  return (
+    <div>
+      {/* //Manejar los filtros de búsqueda */}
+      <div className="bg-gray-800 p-4 rounded-lg shadow-md mb-6">
+        <div className="flex flex-wrap items-center gap-4 justify-between">
+          <div className="flex flex-wrap gap-3">
+            {/* //genero */}
+            <input
+              type="text"
+              name="genero"
+              placeholder="Género"
+              value={filtrosLocales.genero}
+              onChange={handleFiltroChange}
+              className="bg-gray-700 text-white px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            {/* //plataforma */}
+            <input
+              type="text"
+              name="plataforma"
+              placeholder="Plataforma"
+              value={filtrosLocales.plataforma}
+              onChange={handleFiltroChange}
+              className="bg-gray-700 text-white px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            {/* //edad minima */}
+            <input
+              type="number"
+              name="edadMinima"
+              placeholder="Edad mínima"
+              value={filtrosLocales.edadMinima}
+              onChange={handleFiltroChange}
+              className="bg-gray-700 text-white px-4 py-2 rounded-md w-36 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          <div className="flex gap-2 mt-2 sm:mt-0">
+            {/* // Botones para aplicar s */}
+            <button
+              onClick={handleAplicarFiltros}
+              className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md transition-colors hover:cursor-pointer"
+            >
+              Aplicar Filtros
+            </button>
+            {/* // Botón para limpiar los filtros */}
+            <button
+              onClick={handleLimpiarFiltros}
+              className="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded-md transition-colors hover:cursor-pointer"
+            >
+              Limpiar
+            </button>
+          </div>
         </div>
-    );
+      </div>
+
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
+        {videojuegos.map((videojuego) => (// Mapeo de la lista de videojuegos
+          <div
+            key={videojuego._id}
+            className="bg-white rounded-2xl shadow-md hover:shadow-xl overflow-hidden transform hover:scale-105 transition duration-300 relative"
+          >
+            {/* // Imagen del videojuego */}
+            <img
+              src={videojuego.imgUrl}
+              alt={videojuego.titulo}
+              className="h-48 w-full object-cover"
+            />
+
+            <div className="p-4">
+              {/* // Título y detalles del videojuego */}
+              <div className="flex items-center justify-between">
+                <h2 className="text-lg font-bold truncate">{videojuego.titulo}</h2>
+                <Gamepad2 className="text-blue-500" size={24} />
+              </div>
+
+              <p className="text-sm text-gray-600 mt-1">Género: {videojuego.genero.join(', ')}</p>
+              <p className="text-sm text-gray-600">Plataforma: {videojuego.plataforma.join(', ')}</p>
+
+              <div className="mt-2">
+                <span className="inline-block bg-blue-100 text-blue-600 text-xs px-2 py-1 rounded-full">
+                  {videojuego.edadMinima}+ años
+                </span>
+              </div>
+              {/* // Botones de editar y eliminar */}
+              {(perfil.tipo === 'adulto' || perfil.tipo === 'adolescente') && (
+                <div className="mt-4 flex justify-end space-x-2">
+                  <Link
+                    to={`/videojuegos/editar/${videojuego._id}`}
+                    state={{ profile: perfil }}
+                    className="bg-yellow-400 hover:bg-yellow-500 text-white px-3 py-1 rounded-md flex items-center gap-1 text-sm shadow"
+                  >
+                    <Pencil size={16} />
+                    Editar
+                  </Link>
+
+                  <button
+                    onClick={() => handleEliminar(videojuego._id)}
+                    className="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded-md flex items-center gap-1 text-sm shadow hover:cursor-pointer"
+                  >
+                    <Trash2 size={16} />
+                    Eliminar
+                  </button>
+                </div>
+              )}
+              {/* // Botón para agregar a favoritos */}
+              <button
+                onClick={() => handleAddToFavorites(videojuego)}
+                className="text-red-500 mt-2 flex items-center hover:cursor-pointer hover:text-red-800"
+              >
+                <Heart className="mr-1" size={20} /> Agregar a Favoritos
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+
+
+      {/* // Paginación */}
+      <div className="flex justify-center items-center space-x-4 mt-6">
+        {/* // boton de anterior */}
+        <button
+          onClick={() => cambiarPagina(paginaActual - 1)}
+          disabled={paginaActual === 1}
+          className={`px-4 py-2 rounded-md text-white transition-colors ${
+            paginaActual === 1
+              ? 'bg-gray-500 cursor-not-allowed'
+              : 'bg-blue-600 hover:bg-blue-700 hover:cursor-pointer'
+          }`}
+        >
+          Anterior
+        </button>
+        {/* // Información de la paginación */}
+        <span className="text-gray-500">
+          Página <span className="font-bold">{paginaActual}</span> de{' '}
+          <span className="font-bold">{!isNaN(totalPaginas) ? totalPaginas : '1'}</span>
+        </span>
+
+        {/* // boton de siguiente */}
+        <button
+          onClick={() => cambiarPagina(paginaActual + 1)}
+          disabled={paginaActual === totalPaginas}
+          className={`px-4 py-2 rounded-md text-white transition-colors ${
+            paginaActual === totalPaginas
+              ? 'bg-gray-500 cursor-not-allowed'
+              : 'bg-blue-600 hover:bg-blue-700 hover:cursor-pointer'
+          }`}
+        >
+          Siguiente
+        </button>
+      </div>
+
+
+    </div>
+  );
 };
 
 export default VideojuegoCatalogo;
+
+
 
--- src/context/VideojuegoContext.jsx
+++ src/context/VideojuegoContext.jsx
@@ -1,39 +1,68 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-// src/context/VideojuegoContext.jsx
+/* eslint-disable react-refresh/only-export-components */
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { useAuth } from './AuthContext';
 import api from '../services/api';
 import { useLocation } from 'react-router-dom';
 
-const VideojuegoContext = createContext();
+const VideojuegoContext = createContext();// Contexto para videojuegos
 
-// eslint-disable-next-line react-refresh/only-export-components
-export const useVideojuego = () => useContext(VideojuegoContext);
+export const useVideojuego = () => useContext(VideojuegoContext);// Hook para acceder al contexto de videojuegos
 
 export const VideojuegoProvider = ({ children }) => {
-  const { token } = useAuth();
-  const location = useLocation();
-  const { state } = location;
-  const perfil = state?.profile; // Obtiene el perfil del estado de navegación
+  const { token } = useAuth();// Token de autenticación del contexto de autenticación
+  const location = useLocation();// Obtener la ubicación actual
+  const { state } = location;// Obtener el estado de la ubicación actual
+  const perfil = state?.profile;// Obtener el perfil del estado de la ubicación actual
 
-  const [videojuegos, setVideojuegos] = useState([]);
+  const [videojuegos, setVideojuegos] = useState([]);// Estado para almacenar la lista de videojuegos
+  const [paginaActual, setPaginaActual] = useState(1);// Estado para almacenar la página actual
+  const [totalPaginas, setTotalPaginas] = useState(1);// Estado para almacenar el total de páginas
+  const [filtros, setFiltros] = useState({
+    genero: '',
+    plataforma: '',
+    edadMinima: '',
+  });// Estado para almacenar los filtros aplicados
 
-  const fetchVideojuegos = async () => {
+  const fetchVideojuegos = async (pagina = 1) => {// Función para obtener la lista de videojuegos desde la API
     try {
       let filtroEdad = '';
       if (perfil.tipo === 'adolescente') {
         filtroEdad = '13';
       } else if (perfil.tipo === 'infantil') {
         filtroEdad = '7';
-      }
+      }//  condiciones según los tipos de perfil
 
-      const response = await api.get('/videojuegos', {
+      const response = await api.get('/videojuegos', {// Realizar la solicitud GET a la API
         headers: {
           'Authorization': `Bearer ${token}`,
         },
-        params: { edadMinima: filtroEdad },
+        params: {
+          genero: filtros.genero,
+          plataforma: filtros.plataforma,
+          edadMinima: filtros.edadMinima || filtroEdad,
+          pagina,
+          limite: 10,
+        },
       });
-      setVideojuegos(response.data);
+
+      /// Validación robusta del header x-total-count, asegurando que sea un número válido
+      let totalVideojuegos = 0;
+      const headerCount = response.headers['x-total-count'];// Obtener el valor del header x-total-count
+      
+      if (headerCount) {// Verificar si el header existe
+          totalVideojuegos = parseInt(headerCount, 10);// Convertir a número entero
+          if (isNaN(totalVideojuegos)) {// Verificar si es un número válido
+              console.error('Header x-total-count no es número válido:', headerCount);
+              totalVideojuegos = response.data.length; // Fallback al conteo de elementos. 
+          }
+      } else {
+          console.warn('Header x-total-count no presente en la respuesta');// Manejar el caso donde el header no está presente
+          totalVideojuegos = response.data.length; // Fallback al conteo de elementos
+      }
+
+      setVideojuegos(response.data);// Actualizar el estado de videojuegos con los datos obtenidos
+      setTotalPaginas(Math.ceil(totalVideojuegos / 10));// Calcular el total de páginas y actualizar el estado
     } catch (error) {
       console.error('Error al obtener los videojuegos:', error);
     }
@@ -41,19 +70,28 @@ export const VideojuegoProvider = ({ children }) => {
 
   useEffect(() => {
     if (perfil) {
-      fetchVideojuegos();
+      fetchVideojuegos(paginaActual);// Llamar a la función para obtener los videojuegos al cargar el componente
     }
-  }, [perfil, token]);
+  }, [perfil, token, filtros, paginaActual]);// Dependencias para el efecto: perfil, token, filtros y paginaActual
+
+  const aplicarFiltros = (nuevosFiltros) => {
+    setFiltros(nuevosFiltros);
+    setPaginaActual(1); // Reiniciar a la primera página al aplicar filtros
+  };
+
+  const cambiarPagina = (pagina) => {
+    setPaginaActual(pagina);// Cambiar a la página seleccionada
+  };
 
   const crearVideojuego = async (datos) => {
     try {
       await api.post('/videojuegos/crear', datos, {
         headers: {
           'Authorization': `Bearer ${token}`,
-          'Perfil-Id': perfil._id, // Asegúrate de pasar el ID del perfil
+          'Perfil-Id': perfil._id,
         },
       });
-      fetchVideojuegos(); // Actualizar la lista de videojuegos
+      fetchVideojuegos(paginaActual); // Actualizar la lista de videojuegos
     } catch (error) {
       console.error('Error al crear el videojuego:', error);
     }
@@ -64,10 +102,10 @@ export const VideojuegoProvider = ({ children }) => {
       await api.put(`/videojuegos/actualizar/${id}`, datos, {
         headers: {
           'Authorization': `Bearer ${token}`,
-          'Perfil-Id': perfil._id, // Asegúrate de pasar el ID del perfil
+          'Perfil-Id': perfil._id,
         },
       });
-      fetchVideojuegos(); // Actualizar la lista de videojuegos
+      fetchVideojuegos(paginaActual); // Actualizar la lista de videojuegos
     } catch (error) {
       console.error('Error al actualizar el videojuego:', error);
     }
@@ -78,18 +116,20 @@ export const VideojuegoProvider = ({ children }) => {
       await api.delete(`/videojuegos/eliminar/${id}`, {
         headers: {
           'Authorization': `Bearer ${token}`,
-          'Perfil-Id': perfil._id, // Asegúrate de pasar el ID del perfil
+          'Perfil-Id': perfil._id,// Enviar el ID del perfil en la cabecera
         },
       });
-      fetchVideojuegos(); // Actualizar la lista de videojuegos
+      fetchVideojuegos(paginaActual); // Actualizar la lista de videojuegos
     } catch (error) {
       console.error('Error al eliminar el videojuego:', error);
     }
   };
 
   return (
-    <VideojuegoContext.Provider value={{ videojuegos, crearVideojuego, actualizarVideojuego, eliminarVideojuego }}>
+    <VideojuegoContext.Provider value={{ videojuegos, paginaActual, totalPaginas, aplicarFiltros, cambiarPagina, crearVideojuego, actualizarVideojuego, eliminarVideojuego }}>
       {children}
     </VideojuegoContext.Provider>
   );
 };
+
+
